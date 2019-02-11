6c904799ee884d41a6ab157d6173bf89dbac3236
Alex <i_am_good@tut.by>
2019-02-11 17:43:22 +0300

    Project initialization

--- really-simple-tabs.js
+++ really-simple-tabs.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-  const links = $('.rst-tabs-links');
-  const contents = $('.rst-tabs-contents');
-  console.log(links);
-  console.log(contents);
+  // get all links
+  const links = $('.rst-tabs-links > a');
+  // get all tabs contents
+  const contents = $('.rst-tabs-contents > div');
+  // add class to all links
+  links.each(function(i, item) {
+    if (i === 0) {
+      $(item).addClass(`rst-link-${i} rst-link-active`);      
+    } else {
+      $(item).addClass(`rst-link-${i}`);
+    }
+  });
+  // add class to all contents
+  contents.each(function(i, item) {
+    if (i === 0) {
+      $(item).addClass(`rst-content-${i} rst-content-active`);    
+    } else {
+      $(item).addClass(`rst-content-${i}`);  
+    }
+  });
 });
